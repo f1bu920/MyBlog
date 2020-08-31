2f9b639a9f693ff712d633228f8254f81ee56392
f1bu920 <2377478740@qq.com>
2020-08-31 17:08:27 +0800

    用户登录Mapper

--- src/main/java/com/flbu920/blog/Dao/AdminUserMapper.java
+++ src/main/java/com/flbu920/blog/Dao/AdminUserMapper.java
@@ -2,7 +2,7 @@ package com.flbu920.blog.Dao;
 
 import com.flbu920.blog.model.AdminUser;
 
-public interface adminUserDAO {
+public interface AdminUserMapper {
     AdminUser getAdminUserById(Integer id);
 
     AdminUser getAdminUserByNameAndPassword(String loginName, String loginPassword);
