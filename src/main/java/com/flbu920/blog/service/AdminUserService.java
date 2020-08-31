3211c97c3ddc1aeb058f0f7dfc49eb94bda0f53b
f1bu920 <2377478740@qq.com>
2020-08-31 17:09:37 +0800

    AdminUser Service及其实现

--- src/main/java/com/flbu920/blog/service/AdminUserService.java
+++ src/main/java/com/flbu920/blog/service/AdminUserService.java
@@ -1,4 +1,14 @@
 package com.flbu920.blog.service;
 
+import com.flbu920.blog.model.AdminUser;
+
+/**
+ *
+ */
 public interface AdminUserService {
+    AdminUser login(String name,String password);
+
+    AdminUser getAdminUserById(Integer id);
+
+    Boolean updateAdminUser(AdminUser adminUser);
 }
--- src/main/java/com/flbu920/blog/service/Impl/AdminUserServiceImpl.java
+++ src/main/java/com/flbu920/blog/service/Impl/AdminUserServiceImpl.java
@@ -1,4 +1,38 @@
 package com.flbu920.blog.service.Impl;
 
-public class AdminUserServiceImpl {
+import com.flbu920.blog.Dao.AdminUserMapper;
+import com.flbu920.blog.model.AdminUser;
+import com.flbu920.blog.service.AdminUserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class AdminUserServiceImpl implements AdminUserService {
+    @Resource
+    private AdminUserMapper adminUserMapper;
+
+    @Override
+    public AdminUser login(String name, String password) {
+        if (name == null || password == null) {
+            return null;
+        }
+        return adminUserMapper.getAdminUserByNameAndPassword(name, password);
+    }
+
+    @Override
+    public AdminUser getAdminUserById(Integer id) {
+        if (id == null) {
+            return null;
+        }
+        return adminUserMapper.getAdminUserById(id);
+    }
+
+    @Override
+    public Boolean updateAdminUser(AdminUser adminUser) {
+        if (adminUser == null) {
+            return false;
+        }
+        return updateAdminUser(adminUser);
+    }
 }
