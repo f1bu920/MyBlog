2dd9e95b92b239f61007fca428d227078eb51f11
f1bu920 <2377478740@qq.com>
2020-08-31 16:58:11 +0800

    定义登陆token注解

--- src/main/java/com/flbu920/blog/annotation/AdminUserLoginToken.java
+++ src/main/java/com/flbu920/blog/annotation/AdminUserLoginToken.java
@@ -1,4 +1,17 @@
-package com.flbu920.blog.annonation;
+package com.flbu920.blog.annotation;
 
-public interface AdminUserLoginToken {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用于登录验证
+ * @Target(ElementType.METHOD):作用于方法
+ * @Target(ElementType.TYPE):作用于接口、类、枚举、注解
+ */
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AdminUserLoginToken {
+    boolean required() default true;
 }
