7e671789e374e89e532e43830091c1fd4267ccfb
f1bu920 <2377478740@qq.com>
2020-08-31 17:12:51 +0800

    定义向前端返回的数据类型

--- src/main/java/com/flbu920/blog/util/BlogResult.java
+++ src/main/java/com/flbu920/blog/util/BlogResult.java
@@ -1,4 +1,35 @@
 package com.flbu920.blog.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BlogResult {
+    private int code;
+    private String message;
+    private Object data;
+
+    public BlogResult(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static BlogResult success() {
+        return new BlogResult(200, "操作成功");
+    }
+
+    public static BlogResult failure() {
+        return new BlogResult(600, "操作失败");
+    }
+
+    public static BlogResult success(Object data) {
+        return new BlogResult(200, "操作成功", data);
+    }
+
+    public static BlogResult failure(String message) {
+        return new BlogResult(600, message);
+    }
 }
