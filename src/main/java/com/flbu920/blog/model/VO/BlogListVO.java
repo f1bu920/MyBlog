f2fd43a36951be6bf10ac283d7162ece296dd676
f1bu920 <2377478740@qq.com>
2020-09-17 10:07:12 +0800

    添加前端视图对象BlogListVO、SimpleBlogListVO

--- src/main/java/com/flbu920/blog/model/VO/BlogListVO.java
+++ src/main/java/com/flbu920/blog/model/VO/BlogListVO.java
@@ -1,10 +1,16 @@
 package com.flbu920.blog.model.VO;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BlogListVO implements Serializable {
     private Long blogId;
 
--- src/main/java/com/flbu920/blog/model/VO/SimpleBlogListVO.java
+++ src/main/java/com/flbu920/blog/model/VO/SimpleBlogListVO.java
@@ -1,10 +1,14 @@
 package com.flbu920.blog.model.VO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SimpleBlogListVO implements Serializable {
     private Long blogId;
 
